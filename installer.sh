bb5003d888cc387672e03f8d7071fd53600e0a27
emil237 <marianatalaat1@gmail.com>
2023-07-08 22:04:34 +0300

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,87 +1,66 @@
-#!/bin/sh
+#!/bin/bash
 
-# 
-#### Uploaded by: BEKO_73 ###
-### 
-EDIT SCRIPT BY:EMIL_NABIL TO WORK WITH PLUGIN JEDIEPGEXTRAM ###
 # SCRIPT : DOWNLOAD AND INSTALL jediepgextream #
-# 
-# Command: wget https://raw.githubusercontent.com/emilnabil/jediepgextream/main/installer.sh -O - | /bin/sh #
-# 
-#####################
-PACKAGE_DIR='jediepgextream/main'
-MY_IPK="enigma2-plugin-extensions-jediepgxtream_2.3_all.ipk"
-MY_DEB="enigma2-plugin-extensions-jediepgxtream_2.3_all.deb"
-# Auto ... Do not change
 
-# Decide : which package ?
-MY_MAIN_URL="https://raw.githubusercontent.com/emilnabil/"
-if which dpkg > /dev/null 2>&1; then
-	MY_FILE=$MY_DEB
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
-else
-	MY_FILE=$MY_IPK
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
+##setup command=wget https://raw.githubusercontent.com/emil237/jediepgextream/main/installer.sh -O - | /bin/sh
+#
+echo " download and install plugin jediepgextream "
+################################################################################
+version=2.07
+MY_URL="https://raw.githubusercontent.com/emiln237/jediepgextream/main" 
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-overwrite"
+MY_IPK="enigma2-plugin-extensions-jediepgxtream_2.07_all.ipk"
+
+MY_DEB="enigma2-plugin-extensions-jediepgxtream_2.07_all.deb"
+####################################################
+if [ -f /etc/apt/apt.conf ] ; then
+    STATUS='/var/lib/dpkg/status'
+    OS='DreamOS'
+elif [ -f /etc/opkg/opkg.conf ] ; then
+   STATUS='/var/lib/opkg/status'
+   OS='Opensource'
 fi
-MY_TMP_FILE="/tmp/"$MY_FILE
 
-echo ''
-echo 'welcome to jediepgextream'
-echo '**  STARTED  **'                                    
-echo ''
-echo "**********************************************************************"
-# Remove previous file (if any)
-rm -f $MY_TMP_FILE > /dev/null 2>&1
+# remove old version #
+rm -r /usr/lib/enigma2/python/Plugins/Extensions/JediEPGXtream
 
-# Download package file
-echo 'Downloading '$MY_FILE' ...'
-
-echo ''
-wget -T 2 $MY_URL -P "/tmp/"
-
-# Check download
-if [ -f $MY_TMP_FILE ]; then
-	# Install
-	echo ''
-	echo 'Installation started'
-	echo ''
-	if which dpkg > /dev/null 2>&1; then
-		apt-get install --reinstall $MY_TMP_FILE -y
+echo ""
+# Download and install plugin
+cd /tmp
+set -e
+ if which dpkg > /dev/null 2>&1; then
+  wget "https://github.com/emil237/jediepgextream/raw/main/enigma2-plugin-extensions-jediepgxtream_2.07_all.deb"
+		dpkg -i --force-overwrite enigma2-plugin-extensions-jediepgxtream_2.07_all.deb; apt-get install -f -y
+wait
+rm -f enigma2-plugin-extensions-jediepgxtream_2.07_all.deb
 	else
-		opkg install --force-reinstall $MY_TMP_FILE
+  wget "https://github.com/emil237/jediepgextream/raw/main/enigma2-plugin-extensions-jediepgxtream_2.07_all.ipk"
+  wait
+opkg install --force-overwrite enigma2-plugin-extensions-jediepgxtream_2.07_all.ipk
+wait
+rm -f enigma2-plugin-extensions-jediepgxtream_2.07_all.ipk
 	fi
-	MY_EM=$?
-	echo ''
-	echo ''
-	if [ $MY_EM -eq 0 ]; then
-		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
-		echo ''
-echo "
-  *****      *   *        *   *
-  *         * * * *       *   *
-  *****    *   *    *     *   *
-  *.      *           *   *   *
-  *****  *             *  *   ****** "
-
-		echo "   >>>>         RESTARING         <<<<"
-		if which systemctl > /dev/null 2>&1; then
-			sleep 2; systemctl restart enigma2
-		else
-			init 4; sleep 4; init 3;
-		fi
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi;
-	echo ''
-	echo '****************************************'
-	echo '**                   FINISHED                   **'
-	echo '****************************************'
-	echo ''
-	exit 0
-else
-	echo ''
-	echo "Download failed !"
-	exit 1
+echo "================================="
+set +e
+cd ..
+wait
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
 fi
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+		echo "#                Restart Enigma2 GUI                    #"
+echo "#########################################################"
+sleep 2
+if [ $OS = 'DreamOS' ]; then
+    systemctl restart enigma2
+else
+    killall -9 enigma2
+fi
+exit 0
 
-# --------------------------------------------------------------------------------------
+
+
+
